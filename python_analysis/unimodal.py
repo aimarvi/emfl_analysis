af35e72d43bd5e27232fe9e22f84a82f02ab0e1c
Ammar Marvi <ammar.marvi01@gmail.com>
2025-06-25 18:00:47 -0400

    selection method for unimodal

--- python_analysis/unimodal.py
+++ python_analysis/unimodal.py
@@ -26,14 +26,15 @@ hemis = ['rh', 'lh']
 
 vis_rois = ['vwfa', 'FFA', 'STS', 'PPA', 'EBA', 'OFA', 'OPA', 'RSC', 'LOC']
 vis_contrasts = ['W-O', 'Fa-O', 'Fa-O', 'S-O', 'B-O','Fa-O', 'S-O','S-O','O-Scr']
+
 vis_cond = ['Fa', 'S', 'B', 'O', 'W']
 aud_cond = ['FB', 'FP', 'NW', 'QLT', 'MATH']
 
+# only consider top N% of voxels
 top_n_pc = 0.1
 
 cols = ['subject', 'experiment', 'contrast', 'betas', 'hemi', 'parcel']
 df = pd.DataFrame(columns=cols)
-run_sets = ['last'] 
 
 for roi in tqdm(vis_rois):
     for hemi in hemis:
@@ -46,48 +47,49 @@ for roi in tqdm(vis_rois):
             else:
                 parcellation = 'julian_parcels'
                 
-            for runs in run_sets:
-                held_out = 'first'
-                
-                parcel = guts.load_parcel(emfl, parcellation, roi, hemi)
-                contrast = vis_contrasts[vis_rois.index(roi)]
-                effloc_sigs = guts.load_sigs(emfl, 'vis', contrast, runs)
+            parcel = guts.load_parcel(emfl, parcellation, roi, hemi)
+            contrast = vis_contrasts[vis_rois.index(roi)]
 
-                effloc_sigs_parcel = np.squeeze(effloc_sigs[parcel!=0])
-                sorted_indices = np.argsort(effloc_sigs_parcel)[::-1]
+            # select top N% selective voxels, defined in the last 2 runs
+            effloc_sigs = guts.load_sigs(emfl, 'vis', contrast, 'last')
 
-                top_idxs = sorted_indices[:int(np.round(len(sorted_indices)*(top_n_pc)))]
+            effloc_sigs_parcel = np.squeeze(effloc_sigs[parcel!=0])
+            sorted_indices = np.argsort(effloc_sigs_parcel)[::-1]
 
-                # betas from held-out runs of original, visual+audio stimuli
-                alt_betas = guts.load_betas(emfl, 'vis', held_out)
-                for i in range(len(vis_cond)):
-                    betas = alt_betas[:,:,:,i]
-                    betas_parcel = np.squeeze(betas[parcel!=0])
-                    betas_top = np.mean(betas_parcel[top_idxs])
+            top_idxs = sorted_indices[:int(np.round(len(sorted_indices)*(top_n_pc)))]
 
-                    df.loc[len(df)] = {
-                        'subject': emfl,
-                        'experiment': 'emfl',
-                        'contrast': vis_cond[i],
-                        'betas': betas_top,
-                        'hemi': hemi,
-                        'parcel': roi          
-                    }
+            # betas from held-out runs of original, visual+audio stimuli
+            # held-out are the first 3 runs
+            alt_betas = guts.load_betas(emfl, 'vis', 'first')
+            for i in range(len(vis_cond)):
+                betas = alt_betas[:,:,:,i]
+                betas_parcel = np.squeeze(betas[parcel!=0])
+                betas_top = np.mean(betas_parcel[top_idxs])
 
-                # betas from repeat subjects with visual-only stimuli
-                vis_betas = guts.load_betas(vis, 'effloc', 'all')
-                for i in range(len(vis_cond)):
-                    betas = vis_betas[:,:,:,i]
-                    betas_parcel = np.squeeze(betas[parcel!=0])
-                    betas_top = np.mean(betas_parcel[top_idxs])
+                df.loc[len(df)] = {
+                    'subject': emfl,
+                    'experiment': 'emfl',
+                    'contrast': vis_cond[i],
+                    'betas': betas_top,
+                    'hemi': hemi,
+                    'parcel': roi          
+                }
 
-                    df.loc[len(df)] = {
-                        'subject': emfl,
-                        'experiment': 'vis',
-                        'contrast': vis_cond[i],
-                        'betas': betas_top,
-                        'hemi': hemi,
-                        'parcel': roi      
-                    }
+            # betas from repeat subjects with visual-only stimuli
+            # 'all' because we only ran the first 3 runs
+            vis_betas = guts.load_betas(vis, 'effloc', 'all')
+            for i in range(len(vis_cond)):
+                betas = vis_betas[:,:,:,i]
+                betas_parcel = np.squeeze(betas[parcel!=0])
+                betas_top = np.mean(betas_parcel[top_idxs])
+
+                df.loc[len(df)] = {
+                    'subject': emfl,
+                    'experiment': 'vis',
+                    'contrast': vis_cond[i],
+                    'betas': betas_top,
+                    'hemi': hemi,
+                    'parcel': roi      
+                }
 
 df.to_pickle(os.path.join(save_dir, 'uni_EMFL-select.pkl'))
